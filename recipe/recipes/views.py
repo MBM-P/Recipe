d445441914dc8878ff643b243d96ad7d927d24dd
Basma Jarmouni <basmajarmouni0@gmail.com>
2023-05-06 19:05:43 +0200

    completed the crud

--- recipe/recipes/views.py
+++ recipe/recipes/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
+from .forms import RecipeForm
 from .scraper import get_recipes_info
 from .models import Recipe
 
 
 def recipe_list(request):
-    get_recipes_info()
+    #get_recipes_info()
     recipes = Recipe.objects.all()
     return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
+
+def recipe_create(request):
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe = form.save()
+            return redirect('recipe_detail', pk=recipe.pk)
+    else:
+        form = RecipeForm()
+    return render(request, 'recipe_form.html', {'form': form})
+
+def recipe_update(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            recipe = form.save()
+            return redirect('recipe_detail', pk=recipe.pk)
+    else:
+        form = RecipeForm(instance=recipe)
+    return render(request, 'recipe_form.html', {'form': form})
+
+def recipe_delete(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    recipe.delete()
+    return redirect('recipe_list')
