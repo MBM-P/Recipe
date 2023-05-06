f5ebf3e9eb3f01bbe3bb1849b2456fe858d4dfa4
mariam <BLmariam1611@gmail.com>
2023-05-06 18:17:51 +0100

    add pagination

--- recipe/recipes/views.py
+++ recipe/recipes/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from .scraper import get_recipes_info
 from .models import Recipe
-
+from django.core.paginator import Paginator
 
 def recipe_list(request):
-    get_recipes_info()
+    # get_recipes_info()
     recipes = Recipe.objects.all()
-    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+    #set up pagination 
+    p= Paginator(Recipe.objects.all(),2)
+    page = request.GET.get('page')
+    recipe = p.get_page(page)
+    
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes,'recipe':recipe})
